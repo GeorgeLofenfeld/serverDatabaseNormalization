b83379f8d99dba9b5e3abb6897f136ba53fa0be5
GLofenfeld <GLofenfeld@croc.ru>
2024-03-11 00:39:09 +0300

    Добавление newtonsoft и обновление модели юзера

--- serverDatabaseNormalization/Models/UserModel.cs
+++ serverDatabaseNormalization/Models/UserModel.cs
@@ -1,6 +1,17 @@
 namespace serverDatabaseNormalization.Models;
 
+/// <summary>
+/// Пользователь
+/// </summary>
 public class UserModel
 {
+    /// <summary>
+    /// Логин
+    /// </summary>
     public string? Login { get; set; }
+    
+    /// <summary>
+    /// Пароль
+    /// </summary>
+    public string? Password { get; set; }
 }
